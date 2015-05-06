c6f4c4df52216bce8a52e76e667ebaff89b28cd2
Sigita Grinfelde <sigita.grinfelde@gmail.com>
2015-05-06 13:13:00 +0200

    Set up router and first 2 controllers

--- client/lib/app.definition.ts
+++ client/lib/app.definition.ts
@@ -1,9 +1,10 @@
 /// <reference path="../tsd.d.ts" />
 
-angular.module('logtank', ['angular-meteor', 'ngAnimate', 'ngMaterial', 'ui.router']);
-	
-angular.module('logtank').config(['$mdThemingProvider', ($mdThemingProvider: angular.material.MDThemingProvider) => {
-	$mdThemingProvider.theme('default')
-		.primaryPalette('lime')
-		.accentPalette('blue-grey', {'default':'500'});
-}]);	
+angular.module('logtank', ['angular-meteor', 'ngAnimate', 'ngMaterial', 'ui.router'])
+	.config(['$mdThemingProvider', ($mdThemingProvider: angular.material.MDThemingProvider) => {
+		$mdThemingProvider.theme('default')
+			.primaryPalette('lime')
+			.accentPalette('blue-grey', {'default':'500'});
+	}]).config(['$urlRouterProvider', ($urlRouterProvider: angular.ui.IUrlRouterProvider) => {
+		$urlRouterProvider.otherwise('/dashboard');
+	}]);
