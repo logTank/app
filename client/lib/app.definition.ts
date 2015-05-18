2bfdc735f3b6e8481f74722c1d0ce3da24086f3c
Sigita Grinfelde <sigita.grinfelde@gmail.com>
2015-05-18 16:51:52 +0200

    Add material design icons

--- client/lib/app.definition.ts
+++ client/lib/app.definition.ts
@@ -1,15 +1,15 @@
 /// <reference path="../tsd.d.ts" />
-
+module logtank{
 angular.module('logtank', ['angular-meteor', 'ngAnimate', 'ngMaterial', 'ui.router', 'ui.codemirror'])
-        .run(["$rootScope", "$state", ($rootScope: angular.IRootScopeService, $state: angular.ui.IStateService) => {
-              $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
-                // We can catch the error thrown when the $requireUser promise is rejected
-                // and redirect the user back to the main page
-                if (error === "AUTH_REQUIRED") {
-                  $state.go('account.login');
-                }
-              });
-            }])
+    .run(["$rootScope", "$state", ($rootScope: angular.IRootScopeService, $state: angular.ui.IStateService) => {
+          $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+            // We can catch the error thrown when the $requireUser promise is rejected
+            // and redirect the user back to the main page
+            if (error === "AUTH_REQUIRED") {
+              $state.go('account.login');
+            }
+          });
+        }])
 	.config(['$mdThemingProvider', ($mdThemingProvider: angular.material.MDThemingProvider) => {
 		$mdThemingProvider.theme('default')
 			.primaryPalette('lime')
@@ -30,3 +30,22 @@ angular.module('logtank', ['angular-meteor', 'ngAnimate', 'ngMaterial', 'ui.rout
 			}
 		}
 	});
+
+//Full Material icon set can be found here http://google.github.io/material-design-icons/    
+var themeIcons = ['$mdIconProvider' , function ($mdIconProvider) {
+
+$mdIconProvider
+    .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+    .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
+    .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
+    .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
+    .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
+    .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
+    .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
+
+}];
+
+angular.module('logtank')
+    .config(themeIcons);
+
+}
